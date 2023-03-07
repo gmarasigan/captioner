c0d54c4cc1a2bf488ad873d846d68cf12f69ccb2
gmarasigan <92584734+gmarasigan@users.noreply.github.com>
2023-03-07 14:13:48 -0500

    initialize
    added default speech recognition code @ main.java

---
+++ ym.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ym {
+    public static void ym (String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("INPPUT ME: ");
+        String x = input.nextLine();
+
+        System.out.println(x);
+    }
+}
